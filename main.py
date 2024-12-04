55efa54ec56cc0e52197daa8b1a095e444a3bb70
Gleb <glebpetrof2007@gmail.com>
2024-12-04 15:42:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import random
-a=[-3, -5, -2, -12, 0, 15, 4, 7, 2]
+numbers=[-3, -5, -2, -12, 0, 15, 4, 7, 2]
 n=random.randint(4,8)
 m=random.randint(4,8)
 matrix=[]
@@ -7,7 +7,7 @@ for i in range(n):
     matrix.append([0]*m)
 for i in range(n): 
     for j in range(m):
-        matrix[i][j] = a[random.randint(0,7)]
+        matrix[i][j] = numbers[random.randint(0,7)]
 for i in range(len(matrix)):        
     for j in range(len(matrix[i])): 
         print(f"{matrix[i][j]:4}", end = ' ')
@@ -15,6 +15,6 @@ for i in range(len(matrix)):
     sum=0                   
 for i in range(n): 
     for j in range(m):
-        if(matrix[i][j]%3==0):
+        if matrix[i][j]%3==0:
             sum+=matrix[i][j] 
 print(sum)
